cb0f446acee6d21da7c5dfe957d26790d3c227b9
fawad <fawad@FAWAD>
2023-10-25 22:45:41 -0400

    Add project files.

---
+++ 301230271_FawadZaheer_lab3/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace _301230271_FawadZaheer_lab3
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+
+}
---
+++ 301230271_FawadZaheer_lab3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _301230271_FawadZaheer_lab3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
